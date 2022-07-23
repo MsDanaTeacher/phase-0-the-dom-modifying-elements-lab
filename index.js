da3c76d5cbc5f0a76ffe0f7f8b3f511acdc66128
Dana Lobell <danalobell@gmail.com>
2022-07-22 22:56:59 -0400

    commit tree 3b0a273bf64f729aeb6bfc95ae54802edb80813f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+document.querySelector('body').appendChild(newHeader);
+
+newHeader.textContent = "Dana is the champion";
